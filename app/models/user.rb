d6ffa4d0e9fed4f0f356edb57723f59a8fb9b562
Hellen Tamara <hellen.tamara@outlook.com>
2024-03-29 15:43:49 +0900

    added objectives model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :objectives
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
